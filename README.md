b972cc9e5c0b86c418ef53bbc378131043a62f2f
Deepak thapa <dtsuper3@gmail.com>
2021-02-27 00:19:41 +0530

    added new features

--- README.md
+++ README.md
@@ -31,4 +31,4 @@ Pull requests are welcome. For major changes, please open an issue first to disc
 Please make sure to update tests as appropriate
 ## License
 
-[ISC](https://choosealicense.com/licenses/isc/)
+[ISC](https://choosealicense.com/licenses/isc/){:target="_blank"}
--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,4 @@
-import { isNumeric, isEmailValid } from "./utils";
+import { isNumeric, isEmailValid, isPasswordStrong, parseQueryString, allowNumberOnly } from "./utils";
 describe("isNumeric()", () => {
   describe("when input string contains number", () => {
     test("should be true", () => {
@@ -24,3 +24,50 @@ describe("isEmailValid()", () => {
     });
   });
 });
+
+
+describe("isPasswordStrong()", () => {
+  describe("when input valid password, contains min 8 characters, alphabet, number, special character", () => {
+    test("should be true", () => {
+      expect(isPasswordStrong("#Deepak123;")).toBe(true);
+    })
+  })
+
+  describe("when input invalid password", () => {
+    test("should be true", () => {
+      expect(isPasswordStrong("Deepak123")).toBe(false);
+    })
+  })
+})
+
+describe("parseQueryString()", () => {
+  describe('when input query string', () => {
+    test("should parse query string", () => {
+      expect(parseQueryString("q1=1&q2=2")).toMatchObject({ "q1": "1", "q2": "2" });
+    })
+    test("should parse empty query string", () => {
+      expect(parseQueryString("")).toMatchObject({});
+    })
+    test("should parse undefined value", () => {
+      expect(parseQueryString(undefined)).toMatchObject({});
+    })
+  })
+
+})
+
+describe("allowNumberOnly()", () => {
+  describe("when input a keyboard key code", () => {
+    test("should pass when valid", () => {
+      expect(allowNumberOnly(30)).toBe(true);
+    })
+    test("should pass when invalid", () => {
+      expect(allowNumberOnly(37)).toBe(false);
+    })
+    test("should pass when invalid greater than 57", () => {
+      expect(allowNumberOnly(60)).toBe(false);
+    })
+    test("should pass when undefined", () => {
+      expect(allowNumberOnly(undefined)).toBe(false);
+    })
+  })
+})
